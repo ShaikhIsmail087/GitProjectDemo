c930f5da74c72e93a32ac659a53074cb6eda34cd
Shaikhismail087 <shaikhismail0876262@gmail.com>
2023-09-03 18:09:24 +0530

    Commiting Logic to BranchY

---
+++ src/com/app/Logic.java
@@ -0,0 +1,14 @@
+package com.app;
+
+public class Logic {
+
+	public static void main(String[] args) {
+		String hungry="yes";
+		if(hungry=="Yes" ||hungry=="yes" || hungry=="y" || hungry=="Y")
+			System.out.println("eat somthing...");
+		else
+			System.out.println("do work...");
+
+	}
+
+}
